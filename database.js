e395419ea42073b819dc0ea2907fc3c7734d711d
PM-112003 <119692733+PM-112003@users.noreply.github.com>
2025-03-09 17:15:35 +0530

    Update database.js

--- database.js
+++ database.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const mysql = require("mysql2");
 // Create a MySQL connection
 const connection = mysql.createConnection({
-  host: "127.0.0.1", 
+  host: process.env.DB_HOST, 
   user: process.env.DB_USER,      
   password: process.env.SQLPASSWORD, 
 });
